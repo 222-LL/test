a29e30c66fe1213cf5b56d475698388502a45cdc
222-LL <gokukristen@gmail.com>
2020-11-09 10:53:04 +0100

    .gitadd gitignore

--- index.php
+++ index.php
@@ -3,4 +3,15 @@
 
 // GITHUB est ce qu'on appel un hebergeur de repositories GIT
 
-// git init
+// git init 1 fois c tout
+
+// git status
+
+// git config --global user.email "quidelantoine@gmail.com"
+// git config --global user.name "quidelantoine"
+
+// git add index.php
+// git add *
+// git add --all
+
+// git commit -m "init git"
